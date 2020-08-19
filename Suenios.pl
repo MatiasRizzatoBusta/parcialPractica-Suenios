55564e37c1765ad81ee71ca21f17b8b307daa61e
MatiasRizzatoBusta <matias.rizzato@gmail.com>
2020-08-19 18:21:54 -0300

    pto 2

--- Suenios.pl
+++ Suenios.pl
@@ -9,11 +9,11 @@ creeEn(macarena,campanita).
 %diego no cree en nadie asi que no lo pongo para que de falso por universo cerrado.
 
 %suenio(Persona,tipoDeSuenio).
-%ganarLoteria(N1,N2).
+%ganarLoteria([NumerosApostados]).
 %futbolistaEn(Equipo).
 %cantante(DiscosVendidos).
 
-suenio(gabriel,ganarLoteria(5,9)).
+suenio(gabriel,ganarLoteria([5,9])).
 suenio(gabriel,futbolistaEn(arsenal)).
 suenio(juan,cantante(100000)).
 suenio(macarena,cantante(10000)).
@@ -26,4 +26,35 @@ devolver false.
 El otro concepto que entro en juego en este punto es el de polimorfismo ya que me permite tratar de distinta forma a los predicados
 de creeEn y suenio y de esta forma tener todos los suenios de una persona y en quien cree dicha persona.
 */
+
+%equipo(Equipo,Tamanio).
+equipo(arsenal,chico).
+equipo(aldosivi,chico).
+
+esAmbicioso(Persona):-
+    suenio(Persona,_),
+    findall(Dificultad,(suenio(Persona,Suenio),dificultadSuenio(Suenio,Dificultad)),ListaDificultades),
+    sumlist(ListaDificultades,DificultadTotal),    
+    DificultadTotal > 20.
+
+dificultadSuenio(cantante(DiscosVendidos),Dificultad):-
+    DiscosVendidos > 500000,
+    Dificultad is 6.
+
+dificultadSuenio(cantante(DiscosVendidos),Dificultad):-
+    DiscosVendidos =< 500000,
+    Dificultad is 4.
+
+dificultadSuenio(ganarLoteria(ListaDeNumerosApostados),Dificultad):-
+    length(ListaDeNumerosApostados,CantidadDeNumerosApostados),
+    Dificultad is (10 * CantidadDeNumerosApostados).
+
+dificultadSuenio(futbolistaEn(Equipo),Dificultad):-
+    equipo(Equipo,chico),
+    Dificultad is 3.
+
+dificultadSuenio(futbolistaEn(Equipo),Dificultad):-
+    equipo(Equipo,grande),
+    Dificultad is 16.
+
  
